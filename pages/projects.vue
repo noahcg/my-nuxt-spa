f3764f4f7d5d287261b7411e1aba7f09cda80d18
nglushien17 <nglushien@ushmm.org>
2019-07-23 13:35:49 -0400

    created a new projects page

---
+++ pages/projects.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <section class="background">Image Goes Here</section>
+    <section class="content">
+      <h1>Projects</h1>
+      <p>Information about my projects will go here</p>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/variables";
+main {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 0px;
+
+  .background {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+  .content {
+    grid-area: 1 / 2 / 2 / 3;
+  }
+}
+h1 {
+  font-family: $page-title-font;
+  font-size: $page-title-size;
+  font-weight: $page-title-weight;
+  line-height: $page-title-lh;
+}
+</style>
